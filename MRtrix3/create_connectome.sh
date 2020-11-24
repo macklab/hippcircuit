4f96bb1ffe2fc6fb4f37d5ddaa0bf437b2be8788
MelisaGumus <melisa.gumus@mail.utoronto.ca>
2020-11-24 15:42:39 -0500

    add description

--- MRtrix3/create_connectome.sh
+++ MRtrix3/create_connectome.sh
@@ -1,40 +1,25 @@
 # Connectome Generation
 
-work_dir="/data/hippcircuit/derivatives/mrtrix"
+# Custom directories
 bids_dir="/data/hippcircuit"
 sbjs="sub-103212"
 # sbjs=$(head -1 subjects.txt)
 
-# Extract gray matter ROIs from hipp-WM Atlas
-for i in ${sbjs}; do
-  fslmaths ${work_dir}/${i}/${i}_T1w_labels.nii.gz -thr 1 -uthr 1 -bin ${work_dir}/${i}/${i}_T1w_GM_labels
-  atlas=${work_dir}/${i}/${i}_T1w_GM_labels.nii.gz
-  parcel=1
-
-  count=`echo "$parcel+1" | bc`
-  for r in 2 4 5 6 101 102 104 105 106
-  do
-    fslmaths ${work_dir}/${i}/${i}_T1w_labels.nii.gz -thr $r -uthr $r\
-    -bin -mul $count ${work_dir}/${i}/${i}_tmp.nii.gz
-    fslmaths ${work_dir}/${i}/${i}_tmp.nii.gz -binv ${work_dir}/${i}/${i}_tmp2.nii.gz
-    fslmaths $atlas -mas ${work_dir}/${i}/${i}_tmp2.nii.gz -add ${work_dir}/${i}/${i}_tmp.nii.gz $atlas
-    count=`echo "$count+1" | bc`
-  done
-done
-
-# Separate tissue types
+# 1) Structural image processing
+# Tissue segmented image
 for i in ${sbjs}; do
   5ttgen fsl ${bids_dir}/${i}/anat/T1w_acpc_dc_restore_brain.nii.gz\
   ${bids_dir}/derivatives/mrtrix/${i}/5TT.mif -premasked
 done
 
+# Convert multi-tissue image into 3D greyscale
 # Visualize vis.mif for mislabels
 for i in ${sbjs}; do
   5tt2vis ${bids_dir}/derivatives/mrtrix/${i}/5TT.mif\
   ${bids_dir}/derivatives/mrtrix/${i}/vis.mif
 done
 
-## NEW ###
+# Assign the right labels to connectome with respect to the parcellated image
 for i in ${sbjs}; do
   labelconvert ${bids_dir}/derivatives/mrtrix/${i}/${i}_T1w_GM_labels.nii.gz\
   ${bids_dir}/derivatives/mrtrix/${i}/GM_labels_original.txt\
@@ -42,7 +27,7 @@ for i in ${sbjs}; do
   ${bids_dir}/derivatives/mrtrix/${i}/hipp_nodes.mif -force
 done
 
-## NEW
+# Replace sub-cortical gray matter estimates
 for i in ${sbjs}; do
   labelsgmfix ${bids_dir}/derivatives/mrtrix/${i}/hipp_nodes.mif\
   ${bids_dir}/${i}/anat/T1w_acpc_dc_restore_brain.nii.gz\
@@ -50,7 +35,8 @@ for i in ${sbjs}; do
   ${bids_dir}/derivatives/mrtrix/${i}/hipp_nodes_fixSGM.mif -premasked -nthreads 20 -force
 done
 
-## DWI Processing
+# 2) DWI Processing
+# Convert diffusion image into non-compressed format
 for i in ${sbjs}; do
   mrconvert ${bids_dir}/${i}/dwi/data.nii.gz\
   ${bids_dir}/derivatives/mrtrix/${i}/DWI.mif -fslgrad\
@@ -58,11 +44,13 @@ for i in ${sbjs}; do
   -datatype float32 -strides 0,0,0,1
 done
 
+# Generate b=0 image
 for i in ${sbjs}; do
   dwiextract ${bids_dir}/derivatives/mrtrix/${i}/DWI.mif - -bzero | mrmath - mean\
   ${bids_dir}/derivatives/mrtrix/${i}/meanb0.mif -axis 3
 done
 
+# Estimate multi-shell, multi-tissue response function
 for i in ${sbjs}; do
   dwi2response msmt_5tt ${bids_dir}/derivatives/mrtrix/${i}/DWI.mif\
   ${bids_dir}/derivatives/mrtrix/${i}/5TT.mif\
@@ -73,6 +61,7 @@ for i in ${sbjs}; do
 done
 # Check mrview meanb0.mif -overlay.load RF_voxels.mif -overlay.opacity 0.5
 
+# Perform multi-shell, multi-tissue constrained spherical deconvolution
 for i in ${sbjs}; do #msdwi2fod?
   dwi2fod msmt_csd ${bids_dir}/derivatives/mrtrix/${i}/DWI.mif\
   ${bids_dir}/derivatives/mrtrix/${i}/RF_WM.txt\
@@ -84,12 +73,14 @@ for i in ${sbjs}; do #msdwi2fod?
   -mask ${bids_dir}/${i}/dwi/nodif_brain_mask.nii.gz
 done
 
-# Connectome Generation
+# 3) Connectome Generation
+# Mask for gray matter - white matter interface
 for i in ${sbjs}; do
   5tt2gmwmi ${bids_dir}/derivatives/mrtrix/${i}/5TT.mif \
   ${bids_dir}/derivatives/mrtrix/${i}/gmwmi.mif
 done
 
+# Create whole-brain tractography
 for i in ${sbjs}; do
   tckgen ${bids_dir}/derivatives/mrtrix/${i}/WM_FODs.mif\
   ${bids_dir}/derivatives/mrtrix/${i}/10M.tck\
@@ -98,6 +89,7 @@ for i in ${sbjs}; do
   -maxlength 250 -select 10M -cutoff 0.1 -angle 45 -nthreads 30 -force
 done
 
+# Apply spherical-deconvolution informed filtering of tractograms (SIFT) algorithm
 for i in ${sbjs}; do
   tcksift ${bids_dir}/derivatives/mrtrix/${i}/10M.tck\
   ${bids_dir}/derivatives/mrtrix/${i}/WM_FODs.mif\
@@ -105,7 +97,7 @@ for i in ${sbjs}; do
   -act ${bids_dir}/derivatives/mrtrix/${i}/5TT.mif -term_number 2M -nthreads 15
 done
 
-### NEW
+# Create connectome for hipp subfields
 # Use -assignment_forward_search 10 for tracking more fibers
 for i in ${sbjs}; do
   tck2connectome  ${bids_dir}/derivatives/mrtrix/${i}/2M_sift.tck\
@@ -113,8 +105,6 @@ for i in ${sbjs}; do
   ${bids_dir}/derivatives/mrtrix/${i}/${i}_hipp_connectome.csv\
   -out_assignments ${bids_dir}/derivatives/mrtrix/${i}/${i}_hipp_assignments.csv -force -nthreads 30
 done
-
-
 
 
 #### END ####
