b1c5089839d6d306821b8be9f1d6aaf91453818a
MelisaGumus <melisa.gumus@mail.utoronto.ca>
2021-10-22 15:57:29 -0400

    fixed typos

--- itk-snap/extract_ant_post_subfields.sh
+++ itk-snap/extract_ant_post_subfields.sh
@@ -26,14 +26,15 @@ for i in ${sbjs}; do
   fslmaths ${work_dir}/sub-${i}/ant_post_subfields.nii.gz \
   -mas ${work_dir}/sub-${i}/tmp4.nii.gz \
   -add ${work_dir}/sub-${i}/tmp3.nii.gz ${work_dir}/sub-${i}/ant_post_subfields.nii.gz
+done
 
-  atlas=${work_dir}/sub-${i}/ant_post_subfields.nii.gz
-  parcel=1
+atlas=${work_dir}/sub-${i}/ant_post_subfields.nii.gz
+parcel=1
 
-  # Right side
-  countRant=`echo "$parcel+2" | bc`
-  countRpost=`echo "$parcel+6" | bc`
-
+# Right side
+countRant=`echo "$parcel+2" | bc`
+countRpost=`echo "$parcel+6" | bc`
+for i in ${sbjs}; do
   for r in R_subiculum.nii.gz R_CA4DG.nii.gz R_CA2CA3.nii.gz R_stratum.nii.gz; do
     fslmaths ${work_dir}/sub-${i}/${r} -mas ${work_dir}/sub-${i}/Right_Anterior_dil_thr_mask.nii.gz \
     -bin -mul $countRant ${work_dir}/sub-${i}/tmp_ant1.nii.gz
@@ -42,26 +43,28 @@ for i in ${sbjs}; do
     -add ${work_dir}/sub-${i}/tmp_ant1.nii.gz $atlas
     countRant=`echo "$countRant+1" | bc`
 
-    fslmaths ${work_dir}/sub-${i}/${r} -mas Right_Posterior_dil_thr_mask.nii.gz \
-    -bin -mul $countRpost tmp_post1.nii.gz
+    fslmaths ${work_dir}/sub-${i}/${r} -mas ${work_dir}/sub-${i}/Right_Posterior_dil_thr_mask.nii.gz \
+    -bin -mul $countRpost ${work_dir}/sub-${i}/tmp_post1.nii.gz
     fslmaths ${work_dir}/sub-${i}/tmp_post1.nii.gz -binv ${work_dir}/sub-${i}/tmp_post2.nii.gz
     fslmaths $atlas -mas ${work_dir}/sub-${i}/tmp_post2.nii.gz \
     -add ${work_dir}/sub-${i}/tmp_post1.nii.gz $atlas
     countRpost=`echo "$countRpost+1" | bc`
   done
+done
 
-  # Left side
-  countLant=`echo "$parcel+10" | bc`
-  countLpost=`echo "$parcel+15" | bc`
-  for i in L_CA1.nii.gz L_subiculum.nii.gz L_CA4DG.nii.gz L_CA2CA3.nii.gz L_stratum.nii.gz; do
-    fslmaths ${work_dir}/sub-${i}/${r} -mas ${work_dir}/sub-${i}/Left_Anterior_dil_thr_mask.nii.gz \
+# Left side
+countLant=`echo "$parcel+10" | bc`
+countLpost=`echo "$parcel+15" | bc`
+for i in ${sbjs}; do
+  for k in L_CA1.nii.gz L_subiculum.nii.gz L_CA4DG.nii.gz L_CA2CA3.nii.gz L_stratum.nii.gz; do
+    fslmaths ${work_dir}/sub-${i}/${k} -mas ${work_dir}/sub-${i}/Left_Anterior_dil_thr_mask.nii.gz \
     -bin -mul $countLant ${work_dir}/sub-${i}/tmp_ant1.nii.gz
     fslmaths ${work_dir}/sub-${i}/tmp_ant1.nii.gz -binv ${work_dir}/sub-${i}/tmp_ant2.nii.gz
     fslmaths $atlas -mas ${work_dir}/sub-${i}/tmp_ant2.nii.gz \
     -add ${work_dir}/sub-${i}/tmp_ant1.nii.gz $atlas
     countLant=`echo "$countLant+1" | bc`
 
-    fslmaths ${work_dir}/sub-${i}/${r} -mas ${work_dir}/sub-${i}/Left_Posterior_dil_thr_mask.nii.gz \
+    fslmaths ${work_dir}/sub-${i}/${k} -mas ${work_dir}/sub-${i}/Left_Posterior_dil_thr_mask.nii.gz \
     -bin -mul $countLpost ${work_dir}/sub-${i}/tmp_post1.nii.gz
     fslmaths ${work_dir}/sub-${i}/tmp_post1.nii.gz -binv ${work_dir}/sub-${i}/tmp_post2.nii.gz
     fslmaths $atlas -mas ${work_dir}/sub-${i}/tmp_post2.nii.gz \
