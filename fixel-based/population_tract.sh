08f0896477c846b3f89cd620e0a058d67c5d1c69
Melisa Gumus <38737650+MelisaGumus@users.noreply.github.com>
2022-02-21 15:03:51 -0500

    whole brain tck from template

---
+++ fixel-based/population_tract.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -c 40
+#SBATCH --time=3:00:00
+#SBATCH -o %x-%j.out
+
+module load NiaEnv/2018a
+module load intel/2018.2
+module load ants/2.3.1
+module load openblas/0.2.20
+module load fsl/.experimental-6.0.0
+module load fftw/3.3.7
+module load eigen/3.3.4
+module load mrtrix/3.0.0
+module load freesurfer/6.0.0
+
+# Define path
+bids_dir="/project/m/mmack/projects/hippcircuit"
+
+# Define subjects
+sbjs=$(sed -n 1,85p ${bids_dir}/subjects.txt)
+
+# Tissue segmention of T1 template
+5ttgen fsl ${bids_dir}/derivatives/fixel-based/template/T1_template.mif \
+  ${bids_dir}/derivatives/fixel-based/template/5TT_template.mif -premasked -force
+
+# Mask for gray matter - white matter interface
+5tt2gmwmi ${bids_dir}/derivatives/fixel-based/template/5TT_template.mif \
+  ${bids_dir}/derivatives/fixel-based/template/gmwmi_template.mif -force
+
+# Create whole-brain tractography
+tckgen ${bids_dir}/derivatives/fixel-based/template/wmfod_template.mif \
+  ${bids_dir}/derivatives/fixel-based/template/10M_template.tck \
+  -act ${bids_dir}/derivatives/fixel-based/template/5TT_template.mif \
+  -backtrack -crop_at_gmwmi -seed_gmwmi ${bids_dir}/derivatives/fixel-based/template/gmwmi_template.mif \
+  -maxlength 250 -select 10M -cutoff 0.1 -angle 45 -force
+
+# Apply spherical-deconvolution informed filtering of tractograms (SIFT) algorithm
+tcksift ${bids_dir}/derivatives/fixel-based/template/10M_template.tck \
+  ${bids_dir}/derivatives/fixel-based/template/wmfod_template.mif \
+  ${bids_dir}/derivatives/fixel-based/template/2M_sift_template.tck \
+  -act ${bids_dir}/derivatives/fixel-based/template/5TT_template.mif \
+  -term_number 2M -force
