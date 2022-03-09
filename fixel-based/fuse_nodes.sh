67306146d915eeb75a4404f71e29749f636dd834
Melisa Gumus <38737650+MelisaGumus@users.noreply.github.com>
2022-03-08 23:44:29 -0500

    finalize node fusion

--- fixel-based/fuse_nodes.sh
+++ fixel-based/fuse_nodes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -c 40
-#SBATCH --time=30:00
+#SBATCH --time=24:00:00
 #SBATCH -o %x-%j.out
 
 module load NiaEnv/2018a
@@ -22,77 +22,20 @@ sbjs=$(sed -n 1,85p ${bids_dir}/subjects.txt)
 
 # Warp nodes to T1 template
 for i in $sbjs; do
-  mrtransform ${bids_dir}/derivatives/mrtrix/sub-${i}/hipp_nodes.mif \
+  mrtransform ${bids_dir}/derivatives/itk-snap/sub-${i}/MTL_hipp_subfields.nii.gz \
   -warp ${bids_dir}/derivatives/fixel-based/warps/sub-${i}_subject2template_warp.mif \
-  -interp nearest -datatype UInt32LE \
-  ${bids_dir}/derivatives/fixel-based/fixels/sub-${i}/nodes_in_template_space.mif \
-  -template ${bids_dir}/derivatives/mrtrix/sub-${i}/hipp_nodes.mif -force
+  -interp nearest -datatype Float32LE \
+  ${bids_dir}/derivatives/fixel-based/fixels/sub-${i}/MTL_hipp_subfields_in_template_space.mif \
+  -template ${bids_dir}/derivatives/itk-snap/sub-${i}/MTL_hipp_subfields.nii.gz -force
 done
-
-# Convert T1 template to nifti for ants
-mrconvert ${bids_dir}/derivatives/fixel-based/template/T1_template.mif \
-  ${bids_dir}/derivatives/fixel-based/template/T1_template.nii.gz
+#${bids_dir}/derivatives/itk-snap/sub-${i}/MTL_hipp_subfields.nii.gz
 
 # Convert mifs to nifti for ants
 for i in $sbjs; do
-  mrconvert ${bids_dir}/derivatives/fixel-based/fixels/sub-${i}/T1_in_template_space.mif \
-  ${bids_dir}/derivatives/fixel-based/fixels/sub-${i}/T1_in_template_space.nii.gz
-
-  mrconvert ${bids_dir}/derivatives/fixel-based/fixels/sub-${i}/nodes_in_template_space.mif \
-  ${bids_dir}/derivatives/fixel-based/fixels/sub-${i}/nodes_in_template_space.nii.gz
+  mrconvert ${bids_dir}/derivatives/fixel-based/fixels/sub-${i}/MTL_hipp_subfields_in_template_space.mif \
+  ${bids_dir}/derivatives/fixel-based/fixels/sub-${i}/MTL_hipp_subfields_in_template_space.nii.gz -force
 done
 
-# Fuse nodes into a template
-antsJointFusion \
-  -a 0.1 \
-  -g ${bids_dir}/derivatives/fixel-based/fixels/sub-*/T1_in_template_space.nii.gz \
-  -l ${bids_dir}/derivatives/fixel-based/fixels/sub-*/nodes_in_template_space.nii.gz \
-  -b 2.0 \
-  -o ${bids_dir}/derivatives/fixel-based/template/node_template.nii.gz \
-  -s 3x3x3 \
-  -t ${bids_dir}/derivatives/fixel-based/template/T1_template.nii.gz \
-  -v
-
-# OTHER OPTIONS
-# Option 1
-ImageMath 3 ${bids_dir}/derivatives/fixel-based/template/majorityvote.nii.gz \
-  MajorityVoting ${bids_dir}/derivatives/fixel-based/fixels/sub-*/nodes_in_template_space.nii.gz
-# include pearson correlation
-
-# Option 2
-cp ${bids_dir}/derivatives/fixel-based/fixels/sub-792564/nodes_in_template_space.nii.gz \
-  ${bids_dir}/derivatives/fixel-based/template/nodes_mask.nii.gz
-
-tmp_sbjs=$(sed -n 1,30p ${bids_dir}/template_sbjs.txt)
-for i in $tmp_sbjs; do
-  fslmaths ${bids_dir}/derivatives/fixel-based/template/nodes_mask.nii.gz \
-  -add ${bids_dir}/derivatives/fixel-based/fixels/sub-${i}/nodes_in_template_space.nii.gz \
-  ${bids_dir}/derivatives/fixel-based/template/nodes_mask.nii.gz
-done
-
-fslmaths ${bids_dir}/derivatives/fixel-based/template/nodes_mask.nii.gz \
-  -bin ${bids_dir}/derivatives/fixel-based/template/nodes_mask_bin.nii.gz
-
-fslmaths ${bids_dir}/derivatives/fixel-based/template/nodes_mask_bin.nii.gz \
-  -kernel sphere 2.5 -dilM ${bids_dir}/derivatives/fixel-based/template/nodes_mask_bin_dil.nii.gz
-
-antsJointFusion \
-  -a 0.1 \
-  -g ${bids_dir}/derivatives/fixel-based/fixels/sub-10*/T1_in_template_space.nii.gz \
-  -l ${bids_dir}/derivatives/fixel-based/fixels/sub-10*/nodes_in_template_space.nii.gz \
-  -b 2.0 \
-  -o ${bids_dir}/derivatives/fixel-based/template/node_template/node_template_with_mask.nii.gz \
-  -s 3x3x3 \
-  -t ${bids_dir}/derivatives/fixel-based/template/T1_template_mean.nii.gz \
-  -x ${bids_dir}/derivatives/fixel-based/template/nodes_mask_bin_dil.nii.gz \
-  -v
-
-
-
-
-
-
-
-
-
-# END
+# Node template
+ImageMath 3 ${bids_dir}/derivatives/fixel-based/template/node_template.nii.gz \
+  MajorityVoting ${bids_dir}/derivatives/fixel-based/fixels/sub-*/MTL_hipp_subfields_in_template_space.nii.gz
