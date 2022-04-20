4fe0a394ae6be71c397964182dfdd5df8b82b421
Apsis <athan.bourganos@mail.utoronto.ca>
2022-04-20 17:08:39 -0400

    Added test model and verified 3D model type
    Decided to use GLB over FBX and MTL/OBJ. Added a test model with orbit controls.

--- website/hippcircuit-frontend/src/pages/Visualizer.js
+++ website/hippcircuit-frontend/src/pages/Visualizer.js
@@ -1,8 +1,16 @@
 import './Visualizer.css';
 
+import glbtest from '../models/test.glb';
+
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from '@react-three/drei';
+import { Suspense } from "react";
 
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+
+// A test box for trying some orbit controls
 
 function Box() {
     return (
@@ -13,13 +21,27 @@ function Box() {
     )
 }
 
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, glbtest);
+  return (
+    <>
+      <ambientLight color="blue" intensity={1}/>
+      <pointLight position={[10, 20, 10]} intensity={1} color="red"/>
+      <pointLight position={[-10, -20, -10]} intensity={1} color="red"/>
+      <primitive object={gltf.scene} scale={0.1} />
+    </>
+  );
+};
+
 function Visualizer() {
   return (
     <div className="visualizercontainer">
       <div className='vistop'>
           <Canvas>
+            <Suspense fallback={null}>
               <OrbitControls />
-              <Box />
+              <Model />
+            </Suspense>
           </Canvas>
       </div>
       <div className='visbot'>
