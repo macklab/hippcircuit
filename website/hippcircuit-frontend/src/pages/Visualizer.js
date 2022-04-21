7638f96f2ca75a176d75b3a4bfa4a18fb358c1f3
Apsis <athan.bourganos@mail.utoronto.ca>
2022-04-21 17:07:52 -0400

    Visualizer components and added ModelCards + MultiModel
    Added visualizer components Model and ModelCard, added muti-model support, and styled the navbar a bit! More polishing needed and bug fixes required...

--- website/hippcircuit-frontend/src/pages/Visualizer.js
+++ website/hippcircuit-frontend/src/pages/Visualizer.js
@@ -1,51 +1,154 @@
 import './Visualizer.css';
 
 import glbtest from '../models/test.glb';
+import glbtest2 from '../models/test2.glb';
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from '@react-three/drei';
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import Model from '../components/Model';
+import ModelCard from '../components/ModelCard';
 
 
-// A test box for trying some orbit controls
-
-function Box() {
-    return (
-        <mesh>
-            <boxBufferGeometry attach='geometry' />
-            <meshDepthMaterial attach='material' color='green' />
-        </mesh>
-    )
-}
-
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, glbtest);
+const Model1 = () => {
   return (
-    <>
-      <ambientLight color="blue" intensity={1}/>
-      <pointLight position={[10, 20, 10]} intensity={1} color="red"/>
-      <pointLight position={[-10, -20, -10]} intensity={1} color="red"/>
-      <primitive object={gltf.scene} scale={0.1} />
-    </>
-  );
+    <Model model={glbtest} />
+  )
 };
 
+const Model2 = () => {
+  return (
+    <Model model={glbtest2} />
+  )
+};
+
+
+const cardData = [
+  {
+    id: 1,
+    url: Model1,
+    side: 'Left',
+    firstROI: 'CA1',
+    secondROI: 'CA2CA3',
+  },
+  {
+    id: 2,
+    url: Model2,
+    side: 'Right',
+    firstROI: 'CA1',
+    secondROI: 'SUB',
+  },
+  {
+    id: 3,
+    url: Model1,
+    side: 'Left',
+    firstROI: 'CA1',
+    secondROI: 'CA2CA3',
+  },
+  {
+    id: 4,
+    url: Model2,
+    side: 'Right',
+    firstROI: 'CA1',
+    secondROI: 'SUB',
+  },
+  {
+    id: 5,
+    url: Model1,
+    side: 'Left',
+    firstROI: 'CA1',
+    secondROI: 'CA2CA3',
+  },
+  {
+    id: 6,
+    url: Model2,
+    side: 'Right',
+    firstROI: 'CA1',
+    secondROI: 'SUB',
+  },
+  {
+    id: 7,
+    url: Model1,
+    side: 'Left',
+    firstROI: 'CA1',
+    secondROI: 'CA2CA3',
+  },
+  {
+    id: 8,
+    url: Model2,
+    side: 'Right',
+    firstROI: 'CA1',
+    secondROI: 'SUB',
+  },
+  {
+    id: 9,
+    url: Model1,
+    side: 'Left',
+    firstROI: 'CA1',
+    secondROI: 'CA2CA3',
+  },
+  {
+    id: 10,
+    url: Model2,
+    side: 'Right',
+    firstROI: 'CA1',
+    secondROI: 'SUB',
+  },
+  {
+    id: 11,
+    url: Model1,
+    side: 'Left',
+    firstROI: 'CA1',
+    secondROI: 'CA2CA3',
+  },
+  {
+    id: 12,
+    url: Model2,
+    side: 'Right',
+    firstROI: 'CA1',
+    secondROI: 'SUB',
+  },
+  {
+    id: 13,
+    url: Model1,
+    side: 'Left',
+    firstROI: 'CA1',
+    secondROI: 'CA2CA3',
+  },
+  {
+    id: 14,
+    url: Model2,
+    side: 'Right',
+    firstROI: 'CA1',
+    secondROI: 'SUB',
+  },
+];
+
+
 function Visualizer() {
+
+  const [model, setModel] = useState(Model1);
+
   return (
     <div className="visualizercontainer">
       <div className='vistop'>
           <Canvas>
             <Suspense fallback={null}>
               <OrbitControls />
-              <Model />
+              {model}
             </Suspense>
           </Canvas>
       </div>
       <div className='visbot'>
-
+        {cardData.map((card) =>
+          <div key={card.id} onClick={() => setModel(card.url)}>
+            <ModelCard side={card.side} 
+                       firstROI={card.firstROI} 
+                       secondROI={card.secondROI}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
