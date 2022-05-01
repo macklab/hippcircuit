5b459b14c3b5ae9a08f739b0308f53e287c633d8
Melisa Gumus <38737650+MelisaGumus@users.noreply.github.com>
2022-04-30 23:20:27 -0400

    check size in fusion

--- maget/maget_check_fusion.sh
+++ maget/maget_check_fusion.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+#######################
+#### Configuration ####
+#######################
+# Please change these paths for your directory of interest
+maget_dir='/scratch/m/mmack/mmack/projects/hcp_maget'
+fusion_dir='output/fusion/majority_vote'
+#######################
+
+# Load Modules
 if [[ ! $(module is-loaded cobralab/2019b) ]]
 then
 	module load cobralab/2019b
 	source /project/m/mmack/software/MAGeTbrain/bin/activate
 fi
-
-proj_dir='/project/m/mmack/projects/hippcircuit'
-maget_dir='/scratch/m/mmack/mmack/projects/hcp_maget'
-fusion_dir='output/fusion/majority_vote'
 
 labels=$(ls ${maget_dir}/${fusion_dir}/sub-*_labels.mnc)
 
@@ -20,6 +25,4 @@ do
 	then
 		echo $fname
 	fi
-
 done
-	
