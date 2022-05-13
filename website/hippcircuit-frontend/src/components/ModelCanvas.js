6a6cc37868580fd68199a9ad70b9f2980fd06053
Apsis <athan.bourganos@mail.utoronto.ca>
2022-05-13 15:14:15 -0400

    Fixing errors and building the visualizer!
    Changed structure of components, updated manifest.json to stop errors, updated use of React API to follow pattern for React version 18, and built out some visualizer functionality! TODO: Fix loading bug...

---
+++ website/hippcircuit-frontend/src/components/ModelCanvas.js
@@ -0,0 +1,24 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Suspense } from "react";
+
+export default function ModelCanvas(props) {
+
+    const gltf = useLoader(GLTFLoader, props.model);
+
+  return (
+    <Canvas>
+        <Suspense fallback={<></>}>
+            <OrbitControls />
+            <>
+                <ambientLight color="blue" intensity={1}/>
+                <pointLight position={[10, 20, 10]} intensity={0.5} color="white"/>
+                <pointLight position={[-10, -20, -10]} intensity={0.5} color="white"/>
+                <primitive object={gltf.scene} scale={0.1} />
+            </>
+        </Suspense>
+    </Canvas>
+  );
+}
--- website/hippcircuit-frontend/src/pages/Visualizer.js
+++ website/hippcircuit-frontend/src/pages/Visualizer.js
@@ -2,125 +2,111 @@ import './Visualizer.css';
 
 import glbtest from '../models/test.glb';
 import glbtest2 from '../models/test2.glb';
+import glbtest3 from '../models/test3.glb';
 
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from '@react-three/drei';
-import { Suspense, useState } from "react";
+import { useState, useTransition, useEffect } from "react";
 import Carousel from 'react-elastic-carousel'
 import { NavLink } from "react-router-dom";
 
-import Model from '../components/Model';
 import ModelCard from '../components/ModelCard';
-
-
-const Model1 = () => {
-  return (
-    <Model model={glbtest} />
-  )
-};
-
-const Model2 = () => {
-  return (
-    <Model model={glbtest2} />
-  )
-};
+import ModelCanvas from '../components/ModelCanvas';
 
 
 const cardData = [
   {
     id: 1,
-    url: 'Model1',
+    model: glbtest,
     side: 'Left',
     firstROI: 'CA1',
     secondROI: 'CA2CA3',
   },
   {
     id: 2,
-    url: 'Model2',
+    model: glbtest2,
     side: 'Right',
     firstROI: 'CA1',
     secondROI: 'SUB',
   },
   {
     id: 3,
-    url: 'Model1',
+    model: glbtest3,
     side: 'Left',
     firstROI: 'CA1',
-    secondROI: 'CA2CA3',
+    secondROI: 'SUB',
   },
   {
     id: 4,
-    url: 'Model2',
+    model: glbtest3,
     side: 'Right',
     firstROI: 'CA1',
     secondROI: 'SUB',
   },
   {
     id: 5,
-    url: 'Model1',
+    model: glbtest2,
     side: 'Left',
     firstROI: 'CA1',
     secondROI: 'CA2CA3',
   },
   {
     id: 6,
-    url: 'Model2',
+    model: glbtest,
     side: 'Right',
     firstROI: 'CA1',
     secondROI: 'SUB',
   },
   {
     id: 7,
-    url: 'Model1',
+    model: glbtest2,
     side: 'Left',
     firstROI: 'CA1',
     secondROI: 'CA2CA3',
   },
   {
     id: 8,
-    url: 'Model2',
+    model: glbtest,
     side: 'Right',
     firstROI: 'CA1',
     secondROI: 'SUB',
   },
   {
     id: 9,
-    url: 'Model1',
+    model: glbtest2,
     side: 'Left',
     firstROI: 'CA1',
     secondROI: 'CA2CA3',
   },
   {
     id: 10,
-    url: 'Model2',
+    model: glbtest3,
     side: 'Right',
     firstROI: 'CA1',
     secondROI: 'SUB',
   },
   {
     id: 11,
-    url: 'Model1',
+    model: glbtest2,
     side: 'Left',
     firstROI: 'CA1',
     secondROI: 'CA2CA3',
   },
   {
     id: 12,
-    url: 'Model2',
+    model: glbtest,
     side: 'Right',
     firstROI: 'CA1',
     secondROI: 'SUB',
   },
   {
     id: 13,
-    url: 'Model1',
+    model: glbtest3,
     side: 'Left',
     firstROI: 'CA1',
     secondROI: 'CA2CA3',
   },
   {
     id: 14,
-    url: 'Model2',
+    model: glbtest2,
     side: 'Right',
     firstROI: 'CA1',
     secondROI: 'SUB',
@@ -130,90 +116,19 @@ const cardData = [
 
 function Visualizer() {
 
-  const [model, setModel] = useState(Model1);
+  const [model, setModel] = useState(glbtest);
+  const [loaded, setLoaded] = useState(<ModelCanvas model={glbtest} />)
 
-  const [model1, setModel1] = useState(true);
-  const [model2, setModel2] = useState(false);
-  const [model3, setModel3] = useState(false);
-  const [model4, setModel4] = useState(false);
-  const [model5, setModel5] = useState(false);
-  const [model6, setModel6] = useState(false);
-  const [model7, setModel7] = useState(false);
-  const [model8, setModel8] = useState(false);
-  const [model9, setModel9] = useState(false);
-  const [model10, setModel10] = useState(false);
-  const [model11, setModel11] = useState(false);
-  const [model12, setModel12] = useState(false);
-  const [model13, setModel13] = useState(false);
-  const [model14, setModel14] = useState(false);
+  const [isPending, startTransition] = useTransition();
 
   const [titleText, setTitleText] = useState('ROIs: CA1 to CA2CA3')
   const [sideText, setSideText] = useState('Hemisphere: Left')
+  const [roi1, setRoi1] = useState('CA1')
+  const [roi2, setRoi2] = useState('CA2CA3')
 
-
-  const setFalse = (deselected) => {
-    if (deselected === 'Model1') {
-      setModel1(false)
-    } else if (deselected === 'Model2') {
-      setModel2(false)
-    } else if (deselected === 'Model3') {
-      setModel3(false)
-    } else if (deselected === 'Model4') {
-      setModel4(false)
-    } else if (deselected === 'Model5') {
-      setModel5(false)
-    } else if (deselected === 'Model6') {
-      setModel6(false)
-    } else if (deselected === 'Model7') {
-      setModel7(false)
-    } else if (deselected === 'Model8') {
-      setModel8(false)
-    } else if (deselected === 'Model9') {
-      setModel9(false)
-    } else if (deselected === 'Model10') {
-      setModel10(false)
-    } else if (deselected === 'Model11') {
-      setModel11(false)
-    } else if (deselected === 'Model12') {
-      setModel12(false)
-    } else if (deselected === 'Model13') {
-      setModel13(false)
-    } else if (deselected === 'Model14') {
-      setModel14(false)
-    }
-  };
-
-  const setTrue = (selected) => {
-    if (selected === 'Model1') {
-      setModel1(true)
-    } else if (selected === 'Model2') {
-      setModel2(true)
-    } else if (selected === 'Model3') {
-      setModel3(true)
-    } else if (selected === 'Model4') {
-      setModel4(true)
-    } else if (selected === 'Model5') {
-      setModel5(true)
-    } else if (selected === 'Model6') {
-      setModel6(true)
-    } else if (selected === 'Model7') {
-      setModel7(true)
-    } else if (selected === 'Model8') {
-      setModel8(true)
-    } else if (selected === 'Model9') {
-      setModel9(true)
-    } else if (selected === 'Model10') {
-      setModel10(true)
-    } else if (selected === 'Model11') {
-      setModel11(true)
-    } else if (selected === 'Model12') {
-      setModel12(true)
-    } else if (selected === 'Model13') {
-      setModel13(true)
-    } else if (selected === 'Model14') {
-      setModel14(true)
-    }
-  };
+  useEffect(() => {
+    setLoaded(<ModelCanvas model={model} />);
+  }, [model]);
 
   return (
     <div className="visualizercontainer">
@@ -223,9 +138,14 @@ function Visualizer() {
             <h1>{titleText}</h1>
             <h3>{sideText}</h3>
             <h3>N-Tracts: 724</h3>
-            <h3>Stat1: X</h3>
-            <h3>Stat2: Y</h3>
-            <h3>Stat3: Z</h3>
+            <div className='clabel'>
+              <div className='purplebox'></div>
+              <h3>{roi1}</h3>
+            </div>
+            <div className='clabel'>
+              <div className='bluebox'></div>
+              <h3>{roi2}</h3>
+            </div>
           </div>
           <NavLink to='/about'>
             <div className='aboutlink'>
@@ -233,23 +153,20 @@ function Visualizer() {
             </div>
           </NavLink>
           </div>
-          <Canvas>
-            <Suspense fallback={null}>
-              <OrbitControls />
-              {model1 ? <Model1 /> : <></>}
-              {model2 ? <Model2 /> : <></>}
-            </Suspense>
-          </Canvas>
+          {isPending ? <h4>LOADING...</h4> : <></>}
+          {loaded}
       </div>
       <div className='visbot'>
         <Carousel itemsToShow={6}>
         {cardData.map((card) =>
           <div key={card.id} onClick={() => {
-              setTrue(card.url)
-              setFalse(model)
-              setModel(card.url)
+            startTransition(() => {
+              setModel(card.model)
               setTitleText(`ROIs: ${card.firstROI} to ${card.secondROI}`)
               setSideText(`Hemisphere: ${card.side}`)
+              setRoi1(card.firstROI)
+              setRoi2(card.secondROI)
+                })
               }}>
             <ModelCard side={card.side} 
                        firstROI={card.firstROI} 
