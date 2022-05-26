e71c0a31b93d2366fbc53b4af4628f4232d983c4
Apsis <athan.bourganos@mail.utoronto.ca>
2022-05-26 14:05:40 -0400

    Results page update
    Added state management and ability to download files!

--- website/hippcircuit-frontend/src/pages/Results.js
+++ website/hippcircuit-frontend/src/pages/Results.js
@@ -4,9 +4,14 @@ import logo1 from '../images/MackLabLogo.png';
 import logo2 from '../images/BuddingMindsLogo.png';
 import logo3 from '../images/BrainCanadaLogoDark.png';
 import logo4 from '../images/CIHRLogo.jpg';
+import logo5 from '../images/NSERC_logo.png';
+import logo6 from '../images/Vanier_logo.png';
 
 import { NavLink } from "react-router-dom";
+import { useState } from 'react';
 import ROIButton from '../components/ROIButton';
+
+import TestFile from '../downloads/test.zip';
 
 
 const buttondata = [
@@ -15,110 +20,161 @@ const buttondata = [
         side: '',
         firstROI: 'All ROIs',
         secondROI: 'all ROIs',
-        n: 3390
+        n: 3390,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for all ROIs'
       },
       {
         id: 2,
         side: 'Left',
         firstROI: 'CA1',
         secondROI: 'SUB',
-        n: 283
+        n: 283,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Left CA1 to SUB'
       },
       {
         id: 3,
         side: 'Left',
         firstROI: 'CA2CA3',
         secondROI: 'CA1',
-        n: 44
+        n: 44,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Left CA2CA3 to CA1'
       },
       {
         id: 4,
         side: 'Left',
         firstROI: 'CA4DG',
         secondROI: 'CA2CA3',
-        n: 163
+        n: 163,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Left CA4DG to CA2CA3'
       },
       {
         id: 5,
         side: 'Left',
         firstROI: 'ERC',
         secondROI: 'CA1',
-        n: 128
+        n: 128,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Left ERC to CA1'
       },
       {
         id: 6,
         side: 'Left',
         firstROI: 'ERC',
         secondROI: 'CA4DG',
-        n: 0
+        n: 0,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Left ERC to CA4DG'
       },
       {
         id: 7,
         side: 'Left',
         firstROI: 'SUB',
         secondROI: 'CA4DG',
-        n: 83
+        n: 83,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Left SUB to CA4DG'
       },
       {
         id: 8,
         side: 'Left',
         firstROI: 'SUB',
         secondROI: 'ERC',
-        n: 841
+        n: 841,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Left SUB to ERC'
       },
       {
         id: 9,
         side: 'Right',
         firstROI: 'CA1',
         secondROI: 'SUB',
-        n: 616
+        n: 616,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Right CA1 to SUB'
       },
       {
         id: 10,
         side: 'Right',
         firstROI: 'CA2CA3',
         secondROI: 'CA1',
-        n: 108
+        n: 108,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Right CA2CA3 to CA1'
       },
       {
         id: 11,
         side: 'Right',
         firstROI: 'CA4DG',
         secondROI: 'CA2CA3',
-        n: 301
+        n: 301,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Right CA4DG to CA2CA3'
       },
       {
         id: 12,
         side: 'Right',
         firstROI: 'ERC',
         secondROI: 'CA1',
-        n: 110
+        n: 110,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Right ERC to CA1'
       },
       {
         id: 13,
         side: 'Right',
         firstROI: 'ERC',
         secondROI: 'CA4DG',
-        n: 0
+        n: 0,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Right ERC to CA4DG'
       },
       {
         id: 14,
         side: 'Right',
         firstROI: 'SUB',
         secondROI: 'CA4DG',
-        n: 33
+        n: 33,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Right SUB to CA4DG'
       },
       {
         id: 15,
         side: 'Right',
         firstROI: 'SUB',
         secondROI: 'ERC',
-        n: 680
+        n: 680,
+        downloadFile: TestFile,
+        downloadName: 'TestFile1.zip',
+        downloadText: 'Download data for Right SUB to ERC'
       }
 ];
 
 
 function Results() {
+
+  const [roistitle, setRoisTitle] = useState('All ROIs to All ROIs');
+  const [downloadFile, setDownloadFile] = useState(TestFile);
+  const [downloadName, setDownloadName] = useState('Testfile.zip');
+  const [downloadText, setDownloadText] = useState('Download data for all ROIs');
+
   return (
     <div className="resultscontainer">
       <div className='resultsheader'>
@@ -127,23 +183,44 @@ function Results() {
                 <h2>{'<'} BACK</h2>
             </NavLink>
           </div>
+          <h1>Results and Data Download</h1>
       </div>
       <div className='resultsbody'>
+        <div className='resspacerbar'></div>
         <div className='buttonbar'>
             <div className='buttoncollection'>
                 {buttondata.map((button) =>
-                    <div className='buttonflexitem'>
+                    <div className='buttonflexitem' onClick={() => {
+                      setRoisTitle(`${button.side} ${button.firstROI} to ${button.secondROI}`)
+                      setDownloadFile(button.downloadFile)
+                      setDownloadName(button.downloadName)
+                      setDownloadText(button.downloadText)
+                    }}>
                         <ROIButton key={button.id} name={`${button.side} ${button.firstROI} to ${button.secondROI}`} />
                     </div>
                 )}
             </div>
         </div>
+        <div className='resspacerbar'></div>
+        <div className='roititlerow'>
+          <h2>{roistitle}</h2>
+        </div>
+        <div className='downloadrow'>
+          <a href={downloadFile} download={downloadName}>
+            <div className='downloadbutton'>
+              <p>{downloadText}</p>
+            </div>
+          </a>
+        </div>
       </div>
+      <div className='resspacerbar'></div>
       <div className='resultsfooter'>
         <a href='http://macklab.utoronto.ca/'><img className='resultslogo' alt='The Mack Lab logo!' src={logo1} /></a>
         <a href='http://buddingmindslab.utoronto.ca/'><img className='resultslogo' alt='The Budding Minds Lab logo!' src={logo2} /></a>
         <a href='https://braincanada.ca/'><img className='resultslogo' alt='The Brain Canada logo!' src={logo3} /></a>
         <a href='https://cihr-irsc.gc.ca/e/193.html'><img className='resultslogo' alt='The CIHR logo!' src={logo4} /></a>
+        <a href='https://www.nserc-crsng.gc.ca/index_eng.asp'><img className='resultslogo' alt='The NSERC logo!' src={logo5} /></a>
+        <a href='https://vanier.gc.ca/en/home-accueil.html'><img className='resultslogo' alt='The Vanier logo!' src={logo6} /></a>
       </div>
     </div>
   );
