37495b1a4db1688c20544307aef2e622574a49de
Melisa Gumus <melisa.gumus@mail.utoronto.ca>
2024-12-28 12:15:12 -0500

    update logo

--- website/hippcircuit-frontend/src/pages/Home.js
+++ website/hippcircuit-frontend/src/pages/Home.js
@@ -20,11 +20,11 @@ function Home() {
     <div className="container">
         <div className="top">
             <a href='http://macklab.utoronto.ca/'><img className='resultslogo' alt='The Mack Lab logo!' src={logo1} /></a>
-            <a href='http://buddingmindslab.utoronto.ca/'><img className='resultslogo' alt='The Budding Minds Lab logo!' src={logo2} /></a>
+            {/* <a href='http://buddingmindslab.utoronto.ca/'><img className='resultslogo' alt='The Budding Minds Lab logo!' src={logo2} /></a> */}
             <a href='https://braincanada.ca/'><img className='resultslogo' alt='The Brain Canada logo!' src={logo3} /></a>
             <a href='https://cihr-irsc.gc.ca/e/193.html'><img className='resultslogo' alt='The CIHR logo!' src={logo4} /></a>
             <a href='https://www.nserc-crsng.gc.ca/index_eng.asp'><img className='resultslogo' alt='The NSERC logo!' src={logo5} /></a>
-            <a href='https://vanier.gc.ca/en/home-accueil.html'><img className='resultslogo' alt='The Vanier logo!' src={logo6} /></a>
+            <a href='https://vanier.gc.ca/en/home-accueil.html'><img className='resultslogo' alt='The Vanier logo!' src={logo6} /></a> 
             {/* <a href='https://www.blender.org/'><img className='resultslogo' alt='The Blender logo!' src={logo7} /></a> */}
         </div>
         <div className="hleft">
